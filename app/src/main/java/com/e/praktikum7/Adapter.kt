c902a645b9fde26e89a338db543cee21593a4ab1
Pratama Puji <pujiistiani123@gmail.com>
2020-11-30 10:06:40 +0700

    Initial commit

---
+++ app/src/main/java/com/e/praktikum7/Adapter.kt
@@ -0,0 +1,39 @@
+package com.e.praktikum7
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class Adapter(private val mContext: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return if (position == 0) {
+            First.newInstance()
+        }
+        else if(position == 1) {
+            FragmentSecond.newInstance()
+        }
+        else {
+            FragmentThird()
+        }
+
+
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TITLES[position])
+    }
+
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(R.string.tab_text_1,    R.string.tab_text_2, R.string.tab_text_3)
+    }
+}
